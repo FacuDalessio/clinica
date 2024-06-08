a15e2def0e83bc6fad6d0ba09d556123e190e3c9
FacuDalessio <fdalessio22@gmail.com>
2024-06-08 14:09:28 -0300

    home

--- src/app/componentes/bienvenido/bienvenido.component.ts
+++ src/app/componentes/bienvenido/bienvenido.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-bienvenido',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule
+  ],
   templateUrl: './bienvenido.component.html',
   styleUrl: './bienvenido.component.css'
 })
