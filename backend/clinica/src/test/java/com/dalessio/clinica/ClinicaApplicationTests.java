5d2a215a965a84f53238fb046e13223c87f9915a
Facundo Dalessio <fdalessio22@gmail.com>
2026-02-10 14:39:28 -0300

    estructura inicial monolitica para be y fe

---
+++ backend/clinica/src/test/java/com/dalessio/clinica/ClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalessio.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
